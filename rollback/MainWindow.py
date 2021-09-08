543dcab8011b1bc048048962f6eefd768cc9926a
theduckchannel <fredcox@gmail.com>
2021-09-08 20:23:40 -0300

    backup current datetime is: 08092021202340

--- rollback/MainWindow.py
+++ rollback/MainWindow.py
@@ -1,10 +1,18 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets
+from rollback.api import FileUtil
 
 from rollback.main_window import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    app = QtWidgets.QApplication(sys.argv)
+
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.exitPushButton.clicked.connect(self.exitApp)
+
+    def exitApp(self):
+        print(f'Locale dir ====> {FileUtil.getResourcePath()}')
+        self.app.quit()
