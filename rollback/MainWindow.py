39651039a9489e08e4e9a876eddab61cc00db30f
theduckchannel <fredcox@gmail.com>
2021-09-09 08:38:11 -0300

    backup current datetime is: 09092021083811

--- rollback/MainWindow.py
+++ rollback/MainWindow.py
@@ -6,13 +6,18 @@ from rollback.main_window import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    headers = {
+    data = {
         'ID': [],
         'Date': [],
         'User': [],
         'Description': [],
         'Cleanup': []
     }
+
+    commands = {
+        'snapper-list': "snapper list | sed '1,3d'",
+    }
+
     app = QtWidgets.QApplication(sys.argv)
 
     def __init__(self, *args, obj=None, **kwargs):
@@ -25,25 +30,25 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.getSnapshotList()
         self.app.quit()
 
-    @staticmethod
-    def getSnapshotList():
-        cmd = "snapper -c root list| sed '1,2d'"
-        output = sp.getoutput(cmd)
+    def getSnapshotList(self):
+        output = sp.getoutput(self.commands['snapper-list'])
         lines = output.splitlines()
+        self.clearData()  # Clear data array
         for line in lines:
             # print(f'Line ===> {line}')
             # split line
             cols = line.split('|')
             # print(f'cols ====> {cols}')
-            for index, col in enumerate(cols):
-                if index == 0:  # Id
-                    print(f'id ===> {col}')
-                elif index == 3:  # Date
-                    print(f'Date ===> {col}')
-                elif index == 4:  # User
-                    print(f'User ===> {col}')
-                elif index == 5:  # Cleanup
-                    print(f'Cleanup ===> {col}')
-                elif index == 6:  # Description
-                    print(f'Description ===> {col}')
+            self.data['ID'].append(cols[0])
+            self.data['Date'].append(cols[3])
+            self.data['User'].append(cols[4])
+            self.data['Cleanup'].append(cols[5])
+            self.data['Description'].append(cols[6])
 
+        print(f'Data ===> {self.data}')
+
+    def clearData(self):
+        for header in self.data:
+            self.data[header].clear()
+
+        print(self.data)
