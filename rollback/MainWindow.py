025995a27d931a7627e0475f5058ca0edb88e1c5
theduckchannel <fredcox@gmail.com>
2021-09-09 09:04:34 -0300

    backup current datetime is: 09092021090434

--- rollback/MainWindow.py
+++ rollback/MainWindow.py
@@ -24,11 +24,23 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.exitPushButton.clicked.connect(self.exitApp)
+        self.refreshSnapshotsList()
 
     def exitApp(self):
         print(f'Locale dir ====> {FileUtil.getResourcePath()}')
         self.getSnapshotList()
         self.app.quit()
+
+    def refreshSnapshotsList(self):
+        self.getSnapshotList()
+        self.tableWidget.setColumnCount(4)
+        horHeaders = []
+        for n, key in enumerate(self.data.keys()):
+            horHeaders.append(key)
+            for idx, col in enumerate(self.data[key]):
+                print(f'index ===> {idx} data ===> {col}')
+
+        self.tableWidget.setHorizontalHeaderLabels(horHeaders)
 
     def getSnapshotList(self):
         output = sp.getoutput(self.commands['snapper-list'])
@@ -39,16 +51,12 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             # split line
             cols = line.split('|')
             # print(f'cols ====> {cols}')
-            self.data['ID'].append(cols[0])
-            self.data['Date'].append(cols[3])
-            self.data['User'].append(cols[4])
-            self.data['Cleanup'].append(cols[5])
-            self.data['Description'].append(cols[6])
-
-        print(f'Data ===> {self.data}')
+            self.data['ID'].append(cols[0].rstrip())
+            self.data['Date'].append(cols[3].rstrip())
+            self.data['User'].append(cols[4].rstrip())
+            self.data['Cleanup'].append(cols[5].rstrip())
+            self.data['Description'].append(cols[6].rstrip())
 
     def clearData(self):
         for header in self.data:
             self.data[header].clear()
-
-        print(self.data)
