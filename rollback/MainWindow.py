f6bbbe8976ba04c23ca3ea49dcb8f3fc7e74b435
theduckchannel <fredcox@gmail.com>
2021-09-08 23:00:52 -0300

    backup current datetime is: 08092021230052

--- rollback/MainWindow.py
+++ rollback/MainWindow.py
@@ -6,6 +6,13 @@ from rollback.main_window import Ui_MainWindow
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    headers = {
+        'ID': [],
+        'Date': [],
+        'User': [],
+        'Description': [],
+        'Cleanup': []
+    }
     app = QtWidgets.QApplication(sys.argv)
 
     def __init__(self, *args, obj=None, **kwargs):
@@ -22,4 +29,21 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def getSnapshotList():
         cmd = "snapper -c root list| sed '1,2d'"
         output = sp.getoutput(cmd)
-        print(output)
+        lines = output.splitlines()
+        for line in lines:
+            # print(f'Line ===> {line}')
+            # split line
+            cols = line.split('|')
+            # print(f'cols ====> {cols}')
+            for index, col in enumerate(cols):
+                if index == 0:  # Id
+                    print(f'id ===> {col}')
+                elif index == 3:  # Date
+                    print(f'Date ===> {col}')
+                elif index == 4:  # User
+                    print(f'User ===> {col}')
+                elif index == 5:  # Cleanup
+                    print(f'Cleanup ===> {col}')
+                elif index == 6:  # Description
+                    print(f'Description ===> {col}')
+
