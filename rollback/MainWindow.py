aed3054822ed5fe66bfec761ac0d1a2be87bf13c
theduckchannel <fredcox@gmail.com>
2021-09-08 22:34:38 -0300

    backup current datetime is: 08092021223438

--- rollback/MainWindow.py
+++ rollback/MainWindow.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess as sp
 from qtpy import QtWidgets
 from rollback.api import FileUtil
 from rollback.main_window import Ui_MainWindow
@@ -14,4 +15,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def exitApp(self):
         print(f'Locale dir ====> {FileUtil.getResourcePath()}')
+        self.getSnapshotList()
         self.app.quit()
+
+    @staticmethod
+    def getSnapshotList():
+        cmd = "snapper -c root list| sed '1,2d'"
+        output = sp.getoutput(cmd)
+        print(output)
