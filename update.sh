e89d4191536eaddd9fe95f783cc8f7e809d98512
theduckchannel <fredcox@gmail.com>
2021-09-11 09:02:24 -0300

    testando

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
-git config --global user.email "fredcox@gmail.com"
-git config --global user.name "theduckchannel"
-current_date=$(date +"%d%m%Y%H%M%S")
+read -p "Commit message: " COMMITMSG
+echo $COMMITMSG
 git add -A
-git commit -m "backup current datetime is: $current_date"
+git commit -m $COMMITMSG
 git push
