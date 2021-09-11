befbe91d64c1f5eb18367bd4ebae5f2c2774cb69
theduckchannel <fredcox@gmail.com>
2021-09-11 14:24:07 -0300

    ok

--- release.sh
+++ release.sh
@@ -6,9 +6,6 @@ echo "Old Version ====> $OLDVERSION"
 
 read -p "Enter with new version: " NEWVERSION
 
-echo "Updating github repo..."
-update.sh
-
 sed -i "s/^pkgver=.*/pkgver=$NEWVERSION/" ../xerolinux-rollback-git-AUR/PKGBUILD
 sed -i "s/^current_version.*/current_version='$NEWVERSION'/" xerolinux-rollback
 
@@ -21,4 +18,6 @@ git push
 echo "Everything ok $NEWVERSION released!"
 cd ~/xerolinux-rollback-git
 
+echo "Updating github repo..."
+update.sh
 
