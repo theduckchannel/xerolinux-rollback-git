891d671fb835f4e060575e6ba72a97224150c5fb
theduckchannel <fredcox@gmail.com>
2021-09-11 13:55:28 -0300

    Finishing aur release

--- release.sh
+++ release.sh
@@ -9,5 +9,11 @@ read -p "Enter with new version: " NEWVERSION
 sed -i "s/^pkgver=.*/pkgver=$NEWVERSION/" ../xerolinux-rollback-git-AUR/PKGBUILD
 sed -i "s/^current_version.*/current_version='$NEWVERSION'/" xerolinux-rollback
 
+cd ../xerolinux-rollback-git-AUR
+makepkg --printsrcinfo > .SRCINFO
+
 echo "Preparing to release $NEWVERSION...."
+
+
+cd ~/xerolinux-rollback-git
 
