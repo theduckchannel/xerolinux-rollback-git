53c8ea420b0136d919a9ebd543fee1d20797a1e5
theduckchannel <fredcox@gmail.com>
2021-09-11 08:58:14 -0300

    backup current datetime is: 11092021085814

--- release.sh
+++ release.sh
@@ -5,8 +5,9 @@ OLDVERSION=$(grep '^current_version.*' version.py)
 echo "Old Version ====> $OLDVERSION"
 
 read -p "Enter with new version: " NEWVERSION
-echo "You entered $NEWVERSION"
 
 sed -i "s/^pkgver=.*/pkgver=$NEWVERSION/" PKGBUILD
 sed -i "s/^current_version.*/current_version=$NEWVERSION/" version.py
+
+echo "Preparing to release $NEWVERSION...."
 
