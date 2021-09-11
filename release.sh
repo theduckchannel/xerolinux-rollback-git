19823e44a01c5982c75847bca59c65e45059da32
theduckchannel <fredcox@gmail.com>
2021-09-11 08:52:01 -0300

    backup current datetime is: 11092021085201

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+OLDVERSION=$(grep '^current_version.*' version.py)
 
+echo "Old Version ====> $OLDVERSION"
 
+read -p "Enter with new version: " NEWVERSION
+echo "You entered $NEWVERSION"
