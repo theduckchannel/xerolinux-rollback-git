51a8f897982f5c0632d18e2b7839b41eb6d513d5
theduckchannel <fredcox@gmail.com>
2021-09-11 17:05:18 -0300

    release 0.1.28

--- release.sh
+++ release.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 PKGNAME="xerolinux-rollback-git"
+BINARY="xerolinux-rollback"
 CURRENT_DIR=$(pwd)
-OLDVERSION=$(grep '^current_version.*' 'xerolinux-rollback')
+OLDVERSION=$(grep '^current_version.*' $BINARY)
 echo "Old Version ====> $OLDVERSION"
+#####
+# New Version Dialog
 read -p "Enter with new version: " NEWVERSION
-sed -i "s/^pkgver=.*/pkgver=$NEWVERSION/" ../xerolinux-rollback-git-AUR/PKGBUILD
+sed -i "s/^pkgver=.*/pkgver=$NEWVERSION/" ../"$PKGNAME"-AUR/PKGBUILD
 sed -i "s/^current_version.*/current_version='$NEWVERSION'/" xerolinux-rollback
-echo "Preparing to release $NEWVERSION...."
-cd ../xerolinux-rollback-git-AUR
+#####
+echo "Preparing to release $NEWVERSION on AUR...."
+cd "../$PGKNAME-AUR"
 makepkg --printsrcinfo > .SRCINFO
 git add -A
 git commit -m "release $NEWVERSION"
