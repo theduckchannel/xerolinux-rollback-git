be6651332261985d530c1a1b93daae0dd8c8551a
theduckchannel <fredcox@gmail.com>
2021-09-11 13:09:52 -0300

    changes on PKGBUILD no more necessary

--- release.sh
+++ release.sh
@@ -6,7 +6,8 @@ echo "Old Version ====> $OLDVERSION"
 
 read -p "Enter with new version: " NEWVERSION
 
-sed -i "s/^current_version.*/current_version=$NEWVERSION/" version.py
+sed -i "s/^pkgver=.*/pkgver=$NEWVERSION/" ../xerolinux-rollback-git-AUR/PKGBUILD
+sed -i "s/^current_version.*/current_version='$NEWVERSION'/" xerolinux-rollback
 
 echo "Preparing to release $NEWVERSION...."
 
