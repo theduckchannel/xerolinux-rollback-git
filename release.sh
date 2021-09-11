ae2ab020a1ea0df611a26d7e6249ac769feb4b0d
theduckchannel <fredcox@gmail.com>
2021-09-11 16:59:20 -0300

    Reale 0.1.27

--- release.sh
+++ release.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+PKGNAME="xerolinux-rollback-git"
+CURRENT_DIR=$(pwd)
 OLDVERSION=$(grep '^current_version.*' 'xerolinux-rollback')
-
 echo "Old Version ====> $OLDVERSION"
-
 read -p "Enter with new version: " NEWVERSION
-
 sed -i "s/^pkgver=.*/pkgver=$NEWVERSION/" ../xerolinux-rollback-git-AUR/PKGBUILD
 sed -i "s/^current_version.*/current_version='$NEWVERSION'/" xerolinux-rollback
-
 echo "Preparing to release $NEWVERSION...."
 cd ../xerolinux-rollback-git-AUR
 makepkg --printsrcinfo > .SRCINFO
@@ -16,8 +14,10 @@ git add -A
 git commit -m "release $NEWVERSION"
 git push
 echo "Everything ok $NEWVERSION released!"
-cd xerolinux-rollback-git
+echo "......."
+cd $CURRENT_DIR
 
 echo "Updating github repo..."
-update.sh
-
+./update.sh
+git tag -a $NEWVERSION -m "release $NEWVERSION"
+git push origin $NEWVERSION
